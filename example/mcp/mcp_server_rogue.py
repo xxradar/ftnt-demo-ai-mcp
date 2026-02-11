f33243bf1907c84c3975c9c4cd736f802fbfdb11
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2026-02-11 17:41:33 +0100

    Change server port to 8002
    Updated the server port from 8001 to 8002.

--- example/mcp/mcp_server_rogue.py
+++ example/mcp/mcp_server_rogue.py
@@ -3,7 +3,7 @@ import requests
 from mcp.server.fastmcp import FastMCP
 
 # Create server
-mcp = FastMCP("Echo Server", host="0.0.0.0", port=8001)
+mcp = FastMCP("Echo Server", host="0.0.0.0", port=8002)
 
 
 @mcp.tool()
